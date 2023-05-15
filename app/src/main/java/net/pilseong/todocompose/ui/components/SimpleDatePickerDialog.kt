6876ebb681d622e37862dd4b365b62d068a609be
philipheur <heops79@gmail.com>
2023-05-15 09:33:49 +0900

    업데이트 되지 않은 파일 추가 1. DatePicker를 사용한 검색일 범위 기능 추가 2. 불필요한 코드 제거

---
+++ app/src/main/java/net/pilseong/todocompose/ui/components/SimpleDatePickerDialog.kt
@@ -0,0 +1,98 @@
+package net.pilseong.todocompose.ui.components
+
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerFormatter
+import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDateRangePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import net.pilseong.todocompose.R
+import net.pilseong.todocompose.ui.theme.MEDIUM_PADDING
+import java.time.Instant
+import java.time.OffsetDateTime
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun SimpleDatePickerDialog(
+    enabled: Boolean = false,
+    onDismiss: () -> Unit,
+    onConfirmClick: (Long?, Long?) -> Unit
+) {
+    if (enabled) {
+        val dateRangePickerState = rememberDateRangePickerState(
+            initialSelectedStartDateMillis = OffsetDateTime.now().minusDays(7).toInstant()
+                .toEpochMilli(),
+            initialSelectedEndDateMillis = Instant.now().toEpochMilli(),
+            yearRange = IntRange(2000, 2100), // available years
+            initialDisplayMode = DisplayMode.Input
+        )
+
+        DatePickerDialog(
+            onDismissRequest = {
+                onDismiss()
+            },
+            dismissButton = {
+                OutlinedButton(
+                    onClick = { onDismiss() },
+                ) {
+                    Text(text = stringResource(id = R.string.label_no))
+                }
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+//                        Log.i("PHILIP", "${dateRangePickerState.selectedStartDateMillis}")
+//                        Log.i("PHILIP", "${dateRangePickerState.selectedEndDateMillis}")
+//                        if (dateRangePickerState.selectedStartDateMillis != null &&
+//                            dateRangePickerState.selectedEndDateMillis != null
+//                        ) {
+                            onConfirmClick(
+                                dateRangePickerState.selectedStartDateMillis,
+                                dateRangePickerState.selectedEndDateMillis
+                            )
+                            onDismiss()
+//                        } else {
+//                            Toast.makeText(content, "Not Valid Date Range", Toast.LENGTH_SHORT)
+//                                .show()
+//                        }
+                    },
+                ) {
+                    Text(text = stringResource(id = R.string.label_yes))
+                }
+            }
+        ) {
+            DateRangePicker(
+                modifier = Modifier.padding(MEDIUM_PADDING),
+                dateFormatter = DatePickerFormatter(
+                    selectedDateSkeleton = "yy/MM/dd"
+                ),
+                state = dateRangePickerState,
+                title = {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(MEDIUM_PADDING),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Pick up a date range",
+                        )
+                    }
+                },
+            )
+        }
+    }
+}
