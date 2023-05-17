88dfa06cb10d3efe34bb47ffd3b271cb08711f18
philipheur <heops79@gmail.com>
2023-05-18 02:25:47 +0900

    1. note table 생성 2. export 실행 시 alert dialog 생성

---
+++ app/src/main/java/net/pilseong/todocompose/data/model/Notebook.kt
@@ -0,0 +1,19 @@
+package net.pilseong.todocompose.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import net.pilseong.todocompose.util.Constants.NOTE_TABLE
+import java.time.ZonedDateTime
+
+@Entity(tableName = NOTE_TABLE)
+data class Notebook(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    @ColumnInfo(name = "created_at")
+    val createdAt: ZonedDateTime = ZonedDateTime.now(),
+    @ColumnInfo(name = "updated_at")
+    val updatedAt: ZonedDateTime = ZonedDateTime.now()
+)
